9703fd378ade5be507c1c7364097da01c82e9b86
capottier <camille.pottier2@soprasteria.com>
2021-11-09 09:57:10 +0100

    add sorter ni poule

--- src/app/poules/poule/poule.component.ts
+++ src/app/poules/poule/poule.component.ts
@@ -7,9 +7,20 @@ import { Team } from 'src/app/data-model/model/team';
   templateUrl: './poule.component.html',
   styleUrls: ['./poule.component.scss']
 })
-export class PouleComponent {
+export class PouleComponent implements OnInit {
 
   @Input() poule: Poule;
 
-  constructor() { }
+  teams: Team[] = [];
+
+  constructor() {
+  }
+  
+  ngOnInit(): void {
+    this.teams.push(this.poule.team1);
+    this.teams.push(this.poule.team2);
+    this.teams.push(this.poule.team3);
+    this.teams.push(this.poule.team4);
+    this.teams.sort( (team1, team2) => team2.getScore() - team1.getScore())
+  }
 }
